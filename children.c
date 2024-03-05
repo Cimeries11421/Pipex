e82666b869d28caaef2373c00b9fa4d0ccaf03bd
Elodie Briere <ebriere@z4r2p7.42lyon.fr>
2024-03-05 21:43:57 +0100

    Dorian est nul

--- children.c
+++ children.c
@@ -38,13 +38,13 @@ static void	switch_fd(t_pipex *pipex, int id)
 
 static void	cmd_child(t_pipex *pipex, char **env, int id)
 {
-
 	switch_fd(pipex, id);
 	ft_close(pipex, id);
 	if (pipex->entry != -1)
-		close(pipex->entry); //proteger close 
-	
-	dprintf(2, "child : o_path = %s\n", pipex->o_path);
+	{
+		if (close(pipex->entry) == -1)
+			ft_error(pipex, "error close(pipex->entry)", 0, id);
+	}
 	if (pipex->o_path != NULL)
 	{
 		if (execve(pipex->o_path, pipex->cmd, env) == -1)
@@ -61,7 +61,6 @@ static void	cmd_child(t_pipex *pipex, char **env, int id)
 	exit(-1);
 }
 
-
 void	create_children(t_pipex *pipex, int ac, char **av, char **env)
 {
 	int		id;
@@ -70,25 +69,23 @@ void	create_children(t_pipex *pipex, int ac, char **av, char **env)
 	while (pipex->y < (ac - 1))
 	{
 		if (pipe(pipex->fd) == -1)
-			ft_error(pipex,"error pipe", 1, id);
+			ft_error(pipex, "error pipe", 1, id);
 		if (pipex->y == (ac - 2))
 			pipex->last = 1;
-		dprintf(2, "y = %d, av = %s\n", pipex->y, av[pipex->y]);
-		analyse_path(pipex, av[pipex->y], env); //pas besoin de mettre o_path a null ?
+		analyse_path(pipex, av[pipex->y], env);
 		id = fork();
 		if (id == -1)
-		{
-			ft_error(pipex,"error fork", 1, id);
-		}
+			ft_error(pipex, "error fork", 1, id);
 		if (id == 0)
 			cmd_child(pipex, env, id);
 		if (id != 0 && pipex->o_path != NULL)
 			free(pipex->o_path);
 		pipex->cmd = free_tab(pipex->cmd);
-		if (pipex->entry != -1)
-			close(pipex->entry);// close l'ancien fd[0];
+		if (pipex->entry != -1 && close(pipex->entry) == -1)
+			ft_error(pipex, "error close(pipex->entry)", 0, id);
 		pipex->entry = pipex->fd[0];
-		close(pipex->fd[1]);
+		if (close(pipex->fd[1]) == -1)
+			ft_error(pipex, "error close(fd[1])", 0, id);
 		pipex->y++;
 	}
 	ft_close(pipex, id);
